1c9c2aef28388587959a0ef4a0318ec27bc31220
Viraj Patidar <vrj381@gmail.com>
2022-07-22 14:16:55 +0530

    CORS setup

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ const express = require('express');
 const app = express()
 
 
+
+// CORS 
+const cors = require('cors');
+
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:8000']
+}
+
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
+
+
 // Allow JSON in req
 app.use(express.json());
 
